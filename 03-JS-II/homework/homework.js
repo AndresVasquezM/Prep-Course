97d931ebec554f12beeebc5afdf33508f99a9ee2
VaSaN <vasan.modui@gmail.com>
2022-05-10 11:50:40 -0500

    hw up 5 - 31//36

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,17 +181,26 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  const n = 60;
+//  const n = 10;
+//    for (i = 0; i <= n; i++) {
+//      console.log (6 * i);
+  
+let arrayTablaDel6 = []
+for (let i = 0; i < 11; i++) {
+      arrayTablaDel6.push(6 * i)
+}
+return arrayTablaDel6
 
-    for (i = 0; i <= n; i++) {
-      console.log (6 * i);
-  }
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (tieneTresDigitos.lenth === 3){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function doWhile(numero) {
